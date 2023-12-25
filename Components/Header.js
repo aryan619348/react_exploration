e2ce8d39bd50b1a81f6b06fb04e9915657584514
aryan619348 <aryan619348@gmail.com>
2023-12-25 20:33:02 +0530

    started

---
+++ Components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='head'>
+        <h2>Logo</h2>
+        <ul>
+            <li><button className='btt'>Home</button></li>
+            <li><button className='btt'>About</button></li>
+            <li><button className='btt'>Contact</button></li>
+        </ul>
+    </div>
+  )
+}
+
+export default Header
